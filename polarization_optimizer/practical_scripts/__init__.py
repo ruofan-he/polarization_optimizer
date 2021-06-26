2d5a0dcbe89bd4bdf18e962f03ed4619142e43b1
Ruofan He <67919467+ruofan-he@users.noreply.github.com>
2021-06-26 18:57:00 +0900

    append

---
+++ polarization_optimizer/practical_scripts/__init__.py
@@ -0,0 +1,1 @@
+from .random_walk_algo import Random_Walk_Handler
---
+++ polarization_optimizer/practical_scripts/random_walk_algo.py
@@ -0,0 +1,33 @@
+from ..hardware import read_osc_voltage
+from ..hardware import write_channel_volt
+import numpy as np
+
+class Random_Walk_Handler:
+    def __init__(self):
+        self.state_vector = np.array([0.8, 0.8, 0.8], dtype = np.double)
+        self.initial_sigma = 0.1
+        self.alpha = 0.1
+        self.former_voltage = 0.0 #とする
+        self.delta = 0.1 #とする
+
+    def candidate(self):
+        mean = self.state_vector
+        cov = self.initial_sigma * self.alpha * self.delta * np.eye(3)
+        candidate = np.random.multivariate_normal(mean, cov, size=1)[0]
+        return candidate        
+
+    def step(self):
+        candidate = self.candidate()
+        candidate = np.max(np.vstack([candidate, np.ones(3)*0]), axis = 0)
+        candidate = np.max(np.vstack([candidate, np.ones(3)*1.8]), axis = 0)
+        
+        for i, v in enumerate(candidate):
+            write_channel_volt(i, v)
+        
+        new_value = read_osc_voltage()
+        self.delta = abs(new_value - self.former_voltage)
+        self.former_voltage = new_value
+
+        return
+
+        
